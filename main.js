45d2a13693ca1871091c9cadc5099844b29edd07
alicemac93 <alice.maciejewska@gmail.com>
2021-01-16 13:21:14 +0000

    redone mobile version - proper padding missing and bottom part

---
+++ main.js
@@ -0,0 +1,16 @@
+const faqTrigger = document.querySelectorAll(".faq-trigger");
+const faqText = document.querySelectorAll(".faq-text");
+
+const showFaq = (e) => {
+    const triggerParent = e.target.parentNode;
+    for (let i = 0; i < faqText.length; i++){
+        if (faqText[i].parentNode === triggerParent){
+            faqText[i].classList.toggle("active");
+        }
+    }
+}
+
+for (let i = 0; i < faqTrigger.length; i++){
+    faqTrigger[i].addEventListener("click", showFaq);
+    // faqTrigger[i].addEventListener("mouseover", showFaq); KEEP? 
+}
