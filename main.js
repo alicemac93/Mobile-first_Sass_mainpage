500253539c789483df755d68bdd7cfc712a9a5df
alicemac93 <alice.maciejewska@gmail.com>
2021-01-27 16:45:07 +0000

    basic responsivity done, missing mobile nav and few details

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 // faq
 const faqTrigger = document.querySelectorAll(".faq-trigger");
 const faqText = document.querySelectorAll(".faq-text");
+const fawcontainer = document.querySelectorAll(".fi");
+
 
 const showFaq = (e) => {
+    console.log(e.target)
+  
     const triggerParent = e.target.parentNode;
     for (let i = 0; i < faqText.length; i++){
         if (faqText[i].parentNode === triggerParent){
@@ -10,8 +14,10 @@ const showFaq = (e) => {
         }
     }
 }
-for (let i = 0; i < faqTrigger.length; i++){
-    faqTrigger[i].addEventListener("click", showFaq);
+
+
+for (let i = 0; i < fawcontainer.length; i++){
+    fawcontainer[i].addEventListener("click", showFaq);
     // faqTrigger[i].addEventListener("mouseover", showFaq); KEEP? 
 }
 
@@ -33,14 +39,16 @@ function currentSlide(n) {
 function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for ( let i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (let i = 0; i < triggers.length; i++) {
      triggers[i].className = triggers[i].className.replace(" activeTrigger", "");
   }
-  slides[slideIndex-1].style.display = "block";
- triggers[slideIndex-1].className += " activeTrigger";
+  if (window.screen.width > 700) {slides[slideIndex-1].style.display = "inline-flex";}
+  else {slides[slideIndex-1].style.display = "block";}
+    
+    triggers[slideIndex-1].className += " activeTrigger";
 }
 
 for (let i = 0; i < triggers.length; i++){
